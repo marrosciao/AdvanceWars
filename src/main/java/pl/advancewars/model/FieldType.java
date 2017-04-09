c87089746e55c6f77c2124a381f94ab38804708f
tpalucki <tpalucki@gmail.com>
2017-04-09 23:14:33 +0200

    Model update

---
+++ src/main/java/pl/advancewars/model/FieldType.java
@@ -0,0 +1,15 @@
+package pl.advancewars.model;
+
+/**
+ * Created by tpalucki
+ * Created on 09.04.17.
+ */
+public enum FieldType {
+    GRASS,
+    FOREST,
+    MOUNTAIN_1,
+    MOUNTAIN_2,
+    CITY,
+    FACTORY,
+    AIRPORT
+}
---
+++ src/main/java/pl/advancewars/model/MapField.java
@@ -0,0 +1,10 @@
+package pl.advancewars.model;
+
+/**
+ * Created by tpalucki
+ * Created on 09.04.17.
+ */
+public class MapField {
+
+    FieldType type;
+}
