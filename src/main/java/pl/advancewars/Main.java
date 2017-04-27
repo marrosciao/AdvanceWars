b9f0e43178804e51a5273b9afad8b19708228e50
tpalucki <tpalucki@gmail.com>
2017-04-28 00:35:07 +0200

    Dev

--- src/main/java/pl/advancewars/Main.java
+++ src/main/java/pl/advancewars/Main.java
@@ -1,5 +1,7 @@
 package pl.advancewars;
 
+import pl.advancewars.view.GameWindow;
+
 /**
  * Created by tpalucki
  * Created on 07.04.17.
@@ -8,6 +10,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("It's alive!");
+
+        GameWindow window = new GameWindow();
+
     }
 
 }
---
+++ src/main/java/pl/advancewars/controller/GameController.java
@@ -0,0 +1,8 @@
+package pl.advancewars.controller;
+
+/**
+ * Created by tpalucki
+ * Created on 28.04.17.
+ */
+public class GameController {
+}
